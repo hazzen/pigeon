416f1db0e425faeee041938ea0ed3f75e2450df7
Mikey Levine <mrlevine@gmail.com>
2012-03-31 08:14:48 -0700

    found bug in sleep - now can collide no matter the speed

--- js/level.js
+++ js/level.js
@@ -29,39 +29,51 @@ Level.prototype.collides = function(aabb, dx, dy) {
   var xBlocks = [];
   var yBlocks = [];
 
-  aabb.p1.x += dx;
-  aabb.p2.x += dx;
   var ndx = dx;
-  for (var i = this.blocks_.length - 1; i >= 0; --i) {
-    var block = this.blocks_[i].bounds;
-    if (block.overlaps(aabb)) {
-      xBlocks.push(block);
-      if (dx > 0) {
-        ndx = Math.min(ndx, block.p1.x - aabb.p2.x - EPSILON);
-      } else {
-        ndx = Math.max(ndx, block.p2.x - aabb.p1.x + EPSILON);
+  if (dx != 0) {
+    if (dx < 0) {
+      aabb.p1.x += dx;
+    } else {
+      aabb.p2.x += dx;
+    }
+    for (var i = this.blocks_.length - 1; i >= 0; --i) {
+      var block = this.blocks_[i].bounds;
+      if (block.overlaps(aabb)) {
+        xBlocks.push(block);
+        if (dx > 0) {
+          ndx = Math.min(ndx, block.p1.x - aabb.p2.x - EPSILON);
+        } else {
+          ndx = Math.max(ndx, block.p2.x - aabb.p1.x + EPSILON);
+        }
       }
     }
+    aabb.p1.x -= dx - ndx;
+    aabb.p2.x -= dx - ndx;
   }
-  aabb.p1.x -= dx - ndx;
-  aabb.p2.x -= dx - ndx;
-  aabb.p1.y += dy;
-  aabb.p2.y += dy;
+
   var ndy = dy;
-  for (var i = this.blocks_.length - 1; i >= 0; --i) {
-    var block = this.blocks_[i].bounds;
-    if (block.overlaps(aabb)) {
-      yBlocks.push(block);
-      if (dy > 0) {
-        ndy = Math.min(ndy, block.p1.y - aabb.p2.y - EPSILON);
-      } else {
-        ndy = Math.max(ndy, block.p2.y - aabb.p1.y + EPSILON);
+  if (dy != 0) {
+    if (dy < 0) {
+      aabb.p1.y += dy;
+    } else {
+      aabb.p2.y += dy;
+    }
+    for (var i = this.blocks_.length - 1; i >= 0; --i) {
+      var block = this.blocks_[i].bounds;
+      if (block.overlaps(aabb)) {
+        yBlocks.push(block);
+        if (dy > 0) {
+          ndy = Math.min(ndy, block.p1.y - aabb.p2.y - EPSILON);
+        } else {
+          ndy = Math.max(ndy, block.p2.y - aabb.p1.y + EPSILON);
+        }
       }
     }
   }
+
   collisions.xBlocks = xBlocks;
   collisions.yBlocks = yBlocks;
   collisions.dx = ndx;
   collisions.dy = ndy;
   return collisions;
-}
+};
